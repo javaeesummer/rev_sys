859b1bb3752645763b5bdb4b8e8f68c7b7e484f6
周骏迪 <zhoujundi@duc365.com>
2018-07-12 11:19:40 +0800

    first

---
+++ src/main/java/com/rev/revsys/RevSysApplication.java
@@ -0,0 +1,12 @@
+package com.rev.revsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevSysApplication.class, args);
+    }
+}
